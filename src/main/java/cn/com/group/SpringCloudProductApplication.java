2a1b669d8aafa4424fdf5475aadf7c016ea8b320
cec <cec@cec-PC>
2019-03-06 18:54:44 +0800

    1

---
+++ src/main/java/cn/com/group/SpringCloudProductApplication.java
@@ -0,0 +1,13 @@
+package cn.com.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudProductApplication.class, args);
+	}
+
+}
